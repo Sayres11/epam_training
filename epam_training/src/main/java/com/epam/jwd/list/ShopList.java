dcb665f6edfd9629fc6d982ef3c33bc47507bfc8
Aliaksei <44787029+Sayres11@users.noreply.github.com>
2021-07-26 18:21:36 +0300

    Delete epam_training/src/main/java/com/epam/jwd/list directory

--- epam_training/src/main/java/com/epam/jwd/list/ShopList.java
+++
@@ -1,9 +0,0 @@
-package com.epam.jwd.list;
-
-import com.epam.jwd.consturctor.Coffee;
-
-import java.util.ArrayList;
-
-public class ShopList {
-    public ArrayList<Coffee> CoffeeShop = new ArrayList<>();
-}
